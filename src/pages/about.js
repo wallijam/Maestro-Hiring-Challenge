279a438ee4b802f875e9385be4fbdfbbaa8f09ad
James Wallington <wallijam@mail.gvsu.edu>
2020-01-19 11:37:56 -0500

    adding new images and some style fixes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,24 +1,28 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
-    <BackgroundSection
-      img={data.img.childImageSharp.fluid}
-      title=""
-      // leave title blank for now?
-      styleClass="home-background"
-    />
-    <Info />
-  </Layout>
-)
+
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
+      <BackgroundSection
+        img={data.img.childImageSharp.fluid}
+        title=""
+        styleClass="about-background"
+      />
+      <Info />
+    </Layout>
+  )
+}
+
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "olivanders.png" }) {
+    img: file(relativePath: { eq: "about-background.jpg" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
