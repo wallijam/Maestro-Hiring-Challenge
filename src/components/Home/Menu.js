5c302eb83c88205cb74d5b25a7e592912dd064f7
James Wallington <wallijam@mail.gvsu.edu>
2020-01-19 21:18:23 -0500

    adding menu

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -2,22 +2,51 @@ import React, { Component } from "react"
 import Title from "../Globals/Title"
 import Img from "gatsby-image"
 
+// state filtering
 export default class Menu extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      items: [],
+      items: props.items.edges,
       wandItems: props.items.edges,
     }
   }
+
   render() {
     if (this.state.items.length > 0) {
-      return <div>hello from the menu section </div>
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="rare finds" />
+            {/* categories */}
+            {/* items */}
+            <div className="row">
+              {this.state.wandItems.map(({ node }) => {
+                return (
+                  <div
+                    key={node.id}
+                    className="col-8 col-md-6 my-3 d-flex mx-auto"
+                  >
+                    <div>
+                      <Img fixed={node.image.fixed} />
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </section>
+      )
     } else {
       return (
         <section className="menu py-5">
           <div className="container">
-            <Title title="What we got" />
+            <Title title="current treasures"></Title>
+            <div className="row">
+              <div className="col-10 col-sm-6 mx-auto text-center text-capitalize">
+                <h1>Nuthin' here</h1>
+              </div>
+            </div>
           </div>
         </section>
       )
